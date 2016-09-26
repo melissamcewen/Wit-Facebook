0d6f1d935e16c862889c313db2121ab8537f2c90
Melissa Mcewen <melissa.mcewen@rga.com>
2016-09-26 17:06:34 -0500

    moving sessions

--- bot.js
+++ bot.js
@@ -27,32 +27,7 @@ const firstEntityValue = (entities, entity) => {
 };
 
 
-// This will contain all user sessions.
-// Each session has an entry:
-// sessionId -> {fbid: facebookUserId, context: sessionState}
-const sessions = {};
 
-const findOrCreateSession = (fbid) => {
-  let sessionId;
-  // Let's see if we already have a session for the user fbid
-  Object.keys(sessions).forEach(k => {
-    if (sessions[k].fbid === fbid) {
-      // Yep, got it!
-      sessionId = k;
-    }
-  });
-  if (!sessionId) {
-    // No session found for user fbid, let's create a new one
-    sessionId = new Date().toISOString();
-    sessions[sessionId] = {
-      fbid: fbid,
-      context: {
-        _fbid_: fbid
-      }
-    }; // set context, _fid_
-  }
-  return sessionId;
-};
 
 // Bot actions
 const actions = {
