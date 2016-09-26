977c6821f820686c4143408412c655af81c9fec4
Melissa Mcewen <melissa.mcewen@rga.com>
2016-09-26 16:53:38 -0500

    adding send from example

--- bot.js
+++ bot.js
@@ -28,13 +28,39 @@ const firstEntityValue = (entities, entity) => {
 
 // Bot actions
 const actions = {
-  send(request, response) {
+  /*send(request, response) {
     const {sessionId, context, entities} = request;
     const {text, quickreplies} = response;
     return new Promise(function(resolve, reject) {
       console.log('sending...', JSON.stringify(response));
       return resolve();
     });
+  },*/
+  send({sessionId}, {text}) {
+    // Our bot has something to say!
+    // Let's retrieve the Facebook user whose session belongs to
+    //const recipientId = context._fbid_;
+
+    const recipientId = sessions[sessionId].fbid;
+    if (recipientId) {
+      // Yay, we found our recipient!
+      // Let's forward our bot response to her.
+      // We return a promise to let our bot know when we're done sending
+      return fbMessage(recipientId, text)
+      .then(() => null)
+      .catch((err) => {
+        console.error(
+          'Oops! An error occurred while forwarding the response to',
+          recipientId,
+          ':',
+          err.stack || err
+        );
+      });
+    } else {
+      console.error('Oops! Couldn\'t find user for session:', sessionId);
+      // Giving the wheel back to our bot
+      return Promise.resolve()
+    }
   },
   spiderFact({context, entities}) {
     return new Promise(function(resolve, reject) {
@@ -67,7 +93,7 @@ const actions = {
 var accessToken = Config.WIT_TOKEN;
 
 //const client = new Wit({accessToken, actions});
-//interactive(client);
+interactive(client);
 
 const getWit = () => {
   return new Wit({accessToken, actions});
