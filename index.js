d2676e8cfce29de72f232e135f3ee5873d0af065
Melissa Mcewen <melissa.mcewen@rga.com>
2016-09-27 12:08:08 -0500

    resetting to demo

--- index.js
+++ index.js
@@ -14,7 +14,14 @@ const bot = require('./bot.js');
 const Config = require('./const.js');
 const FB = require('./facebook.js');
 
+// Setting up our bot
+const wit = bot.getWit();
+
+// Webserver parameter
+const PORT = process.env.PORT || 8445;
+
 // Wit.ai bot specific code
+
 // This will contain all user sessions.
 // Each session has an entry:
 // sessionId -> {fbid: facebookUserId, context: sessionState}
@@ -42,15 +49,6 @@ const findOrCreateSession = (fbid) => {
   return sessionId;
 };
 
-// Setting up our bot
-const wit = bot.getWit(); 
-
-
-// Webserver parameter
-const PORT = process.env.PORT || 8445;
-
-
-
 // Starting our webserver and putting it all together
 const app = express();
 app.set('port', PORT);
@@ -77,7 +75,7 @@ app.get('/webhook', (req, res) => {
 });
 
 // The main message handler
-/*app.post('/webhook', (req, res) => {
+app.post('/webhook', (req, res) => {
   // Parsing the Messenger API response
   const messaging = FB.getFirstMessagingEntry(req.body);
   if (messaging && messaging.message) {
@@ -133,96 +131,6 @@ app.get('/webhook', (req, res) => {
         }
       );
     }
-  }
-  res.sendStatus(200);
-});*/
-
-// ----------------------------------------------------------------------------
-// Messenger API specific code
-
-// See the Send API reference
-// https://developers.facebook.com/docs/messenger-platform/send-api-reference
-
-const fbMessage = (id, text) => {
-  const body = JSON.stringify({
-    recipient: { id },
-    message: { text },
-  });
-  const qs = 'access_token=' + encodeURIComponent(FB_PAGE_TOKEN);
-  return fetch('https://graph.facebook.com/me/messages?' + qs, {
-    method: 'POST',
-    headers: {'Content-Type': 'application/json'},
-    body,
-  })
-  .then(rsp => rsp.json())
-  .then(json => {
-    if (json.error && json.error.message) {
-      throw new Error(json.error.message);
-    }
-    return json;
-  });
-};
-
-// Message handler
-app.post('/webhook', (req, res) => {
-  // Parse the Messenger payload
-  // See the Webhook reference
-  // https://developers.facebook.com/docs/messenger-platform/webhook-reference
-  const data = req.body;
-
-  if (data.object === 'page') {
-    data.entry.forEach(entry => {
-      entry.messaging.forEach(event => {
-        if (event.message) {
-          // Yay! We got a new message!
-          // We retrieve the Facebook user ID of the sender
-          const sender = event.sender.id;
-
-          // We retrieve the user's current session, or create one if it doesn't exist
-          // This is needed for our bot to figure out the conversation history
-          const sessionId = findOrCreateSession(sender);
-
-          // We retrieve the message content
-          const {text, attachments} = event.message;
-
-          if (attachments) {
-            // We received an attachment
-            // Let's reply with an automatic message
-            fbMessage(sender, 'Sorry I can only process text messages for now.')
-            .catch(console.error);
-          } else if (text) {
-            // We received a text message
-
-            // Let's forward the message to the Wit.ai Bot Engine
-            // This will run all actions until our bot has nothing left to do
-            wit.runActions(
-              sessionId, // the user's current session
-              text, // the user's message
-              sessions[sessionId].context // the user's current session state
-            ).then((context) => {
-              // Our bot did everything it has to do.
-              // Now it's waiting for further messages to proceed.
-              console.log('Waiting for next user messages');
-
-              // Based on the session state, you might want to reset the session.
-              // This depends heavily on the business logic of your bot.
-              // Example:
-              // if (context['done']) {
-              //   delete sessions[sessionId];
-              // }
-
-              // Updating the user's current session state
-              sessions[sessionId].context = context;
-            })
-            .catch((err) => {
-              console.error('Oops! Got an error from Wit: ', err.stack || err);
-            })
-          }
-        } else {
-          console.log('received event', JSON.stringify(event));
-        }
-      });
-    });
   }
   res.sendStatus(200);
 });
